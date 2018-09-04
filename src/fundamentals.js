bfc8e102e27a0eccad2d2d9d14f0b18eb8096e71
Phil B <phil@berryman.org.uk>
2018-09-04 15:22:24 +0100

    src/fundamentals.js

--- src/fundamentals.js
+++ src/fundamentals.js
@@ -4,51 +4,70 @@ function add(a, b){
 }
 
 // Rewrite function as multiline arrow function with return statement
-function addArrow(a, b){
+const addArrow = (a,b) => {
+  return a+b;
   // add the two arguments and return the sum
 }
 
 // Rewrite function as single line arrow function without a return statement
-function addSingleLineArrow(a, b){
+const addSingleLineArrow = (a, b) => a+b;
   // add the two arguments and return the sum
-}
+
 
 // Rewrite function as arrow function
-function remainder(number, divisor){
+const remainder = (number, divisor) => number % divisor;
+
   // return the remainder of number
   // when divided by divisor
-}
+
 
 // Rewrite function as arrow function
-function exponential(number, exponent){
+const exponential = (number, exponent) => number**exponent;
   // this function is passed 2 arguments
   // return first argument to the power of second argument
   // hint: you may need to look up the exponention operator
-}
+
 
 // Rewrite function as arrow function
-function smaller(a, b){
+const smaller = (a, b) => (a !== b) ? Math.min(a,b) : false;
+
   // function receives two numbers as arguments
   // return smaller number
   // if numbers are the same return `false`
-}
+
 
 // Rewrite function as arrow function
-function circumference(diameter){
+const circumference=(diameter) => (Math.PI*diameter).toFixed(3);
   // function receives the diameter
   // calculate the circumference of the circle
   // return number as a string with 3 decimal places
-}
+
 
 // Rewrite function as arrow function
-function countOnes(onesAndZeros){
+const countOnes = (onesAndZeros) => {arr = onesAndZeros.split("");
+  let count=0;
+  for (let i=0;i<arr.length;i++) {
+    if (arr[i] === "1") {
+      count++
+    }
+  }
+  return count;
+}
   // onesAndZeros is a string which contains only `1` and `0`
 
   // return the number of `1`s in the string
-}
+
 
 // Rewrite function as arrow function
-function dateFormat(date){
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+ 
+const dateFormat=(date) => {
+  let dateArr=date.split("-");
+  let year=Number(dateArr[2]);
+  let month=months[Number(dateArr[1])-1];
+  let day=Number(dateArr[0]);
+  return day+" "+month+" "+year;
+}
   // function receives a date as a string
   // in the format yyyy-mm-dd
 
@@ -56,17 +75,23 @@ function dateFormat(date){
   // for example '03-11-2018' becomes '3 November 2018'
 
   // hint: you may want to use the string method `split`.
-}
+
 
 // Rewrite function as arrow function
-function mulitplyAll(numbers){
+const reducer = (multiplier, currentValue) => multiplier * currentValue;
+const mulitplyAll = (numbers) => numbers.reduce(reducer);
   // numbers is an array of numbers
   // multiply all numbers in array
   // and return the product
-}
+
 
 // Rewrite function as arrow function
-function largestArea(rectangles){
+const largestArea= rectangles => {
+    let areas=[]
+    for (i=0; i<rectangles.length;i++) {
+        areas[i]= rectangles[i][0]*rectangles[i][1];
+    }
+    return Math.max(...areas);
   // rectangles is an array of arrays,
   // where the inner contains two numbers
   // the first number is the height of a rectangle and
